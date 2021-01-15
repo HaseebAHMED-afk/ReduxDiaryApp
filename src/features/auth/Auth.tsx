10a469c394dea55cc36895cf7ae9a265ec74d8a6
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-01-15 21:26:09 +0500

    At Auth

--- src/features/auth/Auth.tsx
+++ src/features/auth/Auth.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React , {FC , useState} from 'react'
 import { useForm } from 'react-hook-form'
 import { User } from '../../interfaces/user.interface'
@@ -23,9 +24,55 @@ const Auth :FC = () => {
     const { handleSubmit,register,errors } = useForm<User>({
         resolver: yupResolver(schema)
     })
+
+    const [isLogin , setIsLogin] = useState(true);
+    const [loading, setLoading] = useState(false);
+    const dispatch = useAppDispatch();
+
+    const submitForm = ( data: User) => {
+        const path = isLogin ? '/auth/login' : '/auth/signup';
+        http
+        .post<User, AuthResponse>(path,data)
+        .then((res) => {
+            const { user,token } = res;
+            dispatch(saveToken(token))
+            dispatch(setUser(user))
+            dispatch(setAuthState(true))
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+        .finally(() => {
+            setLoading(false)
+        })
+    }
+    
+
     return (
-        <div>
-        
+        <div className="auth">
+            <div className="card">
+                <form onSubmit={handleSubmit(submitForm)} >
+                    <div className="inputWrapper">
+                        <input type="text" ref={register} name="username" placeholder="Username" />
+                        {
+                            errors && errors.username && (
+                                <p className="error">{errors.username.message}</p>
+                            )
+                        }
+                    </div>
+                    <div className="inputWrapper">
+                        <input type="password" ref={register} name="password" placeholder="Password" />
+                        {
+                            errors && errors.password && (
+                                <p className="error">{errors.password.message}</p>
+                            )
+                        }
+                    </div>
+                    {
+                        !isLogin 
+                    }
+                </form>
+            </div>
         </div>
     )
 }
